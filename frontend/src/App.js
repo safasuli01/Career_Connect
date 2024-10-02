90a81f1ea162c6752d3a20fb0bd9fecefe82308c
gihannazmy <gihanelsayed187@gmail.com>
2024-10-02 12:20:50 +0300

    Remove .pyc files and update .gitignore

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+from useEffect from 'react';
 import AppRoutes from './routes/AppRoute';
+import axios from 'axios';
 
 function App() {
+    const endpoint = `${import.meta.env.REACT_API_URL}/posts/`
+    const fetchData = async() => {
+    console.log('fetching..')
+    const response = await.axios.get(endpoint)
+    const {data} = response
+    return data
+    }
+    useEffect(() => {
+    fetchData()
+    }, [])
   return (
     <div>
       <AppRoutes />
