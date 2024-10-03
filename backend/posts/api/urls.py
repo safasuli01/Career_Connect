4f97914da70a49e9bc31f3ec9f351baa85b417b6
safasuli01 <safa.abduallah2001@hotmail.com>
2024-10-03 22:46:36 +0300

    remove backend

--- backend/posts/api/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import JobViewSet, ProjectViewSet
-
-post_router = DefaultRouter()
-post_router.register(r'jobs', JobViewSet)
-post_router.register(r'projects', ProjectViewSet)
-
-urlpatterns = post_router.urls
